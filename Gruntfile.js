9f34e8fc62fe2999f8f808f802c6e52cfa48e97b
ant <ant@maxant.co.uk>
2016-06-16 22:21:14 +0200

    updated

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,11 +5,13 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     webpack: {
       somename: require('./webpack.config.js')
+    },
+    clean: {
+        dev: ['*.js', '!webpack.config.js', '!Gruntfile.js']
     }
   });
 
   grunt.loadNpmTasks('grunt-webpack');
-
   grunt.loadNpmTasks('grunt-contrib-clean');
 
   // Default task(s).
